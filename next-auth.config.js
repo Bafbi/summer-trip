3379f55b6c2c7ffa30f196e6b972ff892875dff3
Paul POUSSET <paul.pousset@student.junia.com>
2023-05-10 16:48:00 +0200

    group page + base page

---
+++ next-auth.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  pages: {
+    signIn: '/auth/signin',
+    signOut: '/auth/signout',
+    error: '/auth/error',
+    verifyRequest: '/auth/verify-request',
+    newUser: '/auth/new-user'
+  }
+};
--- src/pages/g/index.tsx
+++ src/pages/g/index.tsx
@@ -1,33 +1,105 @@
-import { type NextPage } from "next";
+import { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import { useSession, signOut } from "next-auth/react";
 import { api } from "~/utils/api";
+import { useRouter } from "next/router";
+import { redirect } from "next/dist/server/api-utils";
+import { DropdownMenu } from "~/components/menu";
+import { useState } from "react";
+import Image from "next/image";
 
 const GroupsPage: NextPage = () => {
+  const { data: sessionData } = useSession({ required: true });
+  const { data: groupsData, isLoading: groupsLoading } =
+    api.group.getAll.useQuery();
+  const router = useRouter();
 
-    const {data: groupsData, isLoading: groupsLoading} = api.group.getAll.useQuery();
+  const handleLogout = async () => {
+    await signOut({ redirect: true, callbackUrl: "/" });
+    // redirect("/");
+    // router.push(""); // Redirige vers la page de connexion après déconnexion
+  };
 
-    if (groupsLoading) return <div>Loading...</div>
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-    if (!groupsData) return <div>Something went wrong</div>
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  if (groupsLoading) return <div>Loading...</div>;
+  if (!groupsData) return <div>Something went wrong</div>;
 
   return (
     <>
       <Head>
         <title>Groups</title>
       </Head>
-      <main>
-        <div className="grid row-span-2">
+
+      <header className="flex items-center bg-[#1E5552] w-screen p-2 justify-between min-h-fit">
+        <button className="">
+        {sessionData?.user.image ? (
+              <Image
+                className="rounded-full w-fit h-fit"
+                src={"/judog.PNG"}
+                alt="User Logo"
+                width={64}
+                height={64}
+              />
+            ) : (
+              "Sign in"
+            )} 
+
+        </button>
+        <h1 className="flex text-[#E49A0A] font-bold text-xl">
+          {sessionData?.user?.name}
+        </h1>
+        
+        <button
+        
+          className="w-fit h-fit"
+          onClick={toggleDropdown}
+        >
+          {sessionData?.user.image ? (
+              <Image
+                className="rounded-full w-fit h-fit"
+                src={sessionData.user.image}
+                alt="User Logo"
+                width={64}
+                height={64}
+              />
+            ) : (
+              "Sign in"
+            )}
+        </button>
+        {isDropdownOpen && <DropdownMenu />}
+      </header>
+
+      <main className="min-h-screen bg-[#40534D]">
+        <div className=" grid grid-cols-2 gap-6 p-4">
+          <Link href="/create-group">
+            <div className=" flex flex-col items-center justify-center gap-2 rounded-lg bg-[white] px-2 p-4">
+              <div className="flex h-full flex-col items-center justify-end">
+                <img src="/plus.png" alt="Add Group" className="h-9 w-9 " />
+                <h1 className="mt-2 text-xs ">  
+                  Create Group
+                </h1>
+              </div>
+            </div>
+          </Link>
           {groupsData.map((group) => {
             return (
               <Link key={group.id} href={`/g/${group.id}`}>
-                <div className="flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-                  <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-                    {group.id}
-                  </h1>
+                <div className=" flex flex-col items-center justify-center gap-2 rounded-lg bg-white px-2 p-4">
+                  <div className="flex h-full flex-col items-center justify-end">
+                    <div className="h-8 w-8 rounded-full bg-[#1CCDB3]"></div>
+                    <h1 className="mt-2 text-xs ">
+                      {group.id}
+                    </h1>
+                  </div>
                 </div>
               </Link>
-            )
+            );
           })}
         </div>
       </main>
