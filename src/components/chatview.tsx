c43e6a2d992255d38df34c478a145d05ce0cf485
Bafbi <bafbi3@gmail.com>
2023-05-11 12:28:33 +0200

    dynamic import for main page app

---
+++ src/components/chatview.tsx
@@ -0,0 +1,18 @@
+import { api } from "~/utils/api";
+
+export const ChatView = (props: { groupId: string }) => {
+  const groupId = props.groupId;
+
+  const { data: groupData, isLoading: groupLoading } =
+    api.group.getById.useQuery({ id: groupId });
+
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+          {groupLoading ? <div>a{groupId}a Loading...</div> : groupData.name}
+        </h1>
+      </div>
+    </div>
+  );
+};
--- src/pages/g/[id].tsx
+++ src/pages/g/[id].tsx
@@ -1,31 +1,61 @@
 import { type NextPage } from "next";
+import dynamic from "next/dynamic";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import Header from "~/components/layout/header";
 import { api } from "~/utils/api";
 
 const GroupPage: NextPage = () => {
 
+  const [ page, setPage ] = useState("chat");
+
+  useEffect(() => {
+    const last_page = localStorage.getItem("last_page");
+    if (last_page) {
+      setPage(last_page);
+    }
+  }, []);
+
+  const setSelectedPage = (page: string) => {
+    localStorage.setItem("last_page", page);
+    setPage(page);
+  };
+
+  const ChatComponent = dynamic(() => import("~/components/chatview"), {
+    loading: () => <p>Loading...</p>,
+    ssr: false
+  });
+
   const id = useRouter().query.id as string;
 
   const { data: groupData, isLoading: groupLoading } = api.group.getById.useQuery({ id });
 
-  if (groupLoading) return <div>a{id}a Loading...</div>;
   if (!groupData) return <div>Something went wrong</div>;
+
 
   return (
     <>
       <Head>
-        <title>{groupData.name}</title>
+        <title>{page}</title>
       </Head>
       <Header />
       <main>
-        <div className="flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            {groupData.name}
-          </h1>
-        </div>
+        <ChatComponent groupId={id} />
       </main>
+      <footer>
+        <button
+          className="h-12 w-12"
+          onClick={() => setSelectedPage("like")}>
+          Like
+        </button>
+        <button
+          className="h-12 w-12"
+          onClick={() => setSelectedPage("chat")}>
+          Chat
+        </button>
+
+      </footer>
     </>
   );
 };
