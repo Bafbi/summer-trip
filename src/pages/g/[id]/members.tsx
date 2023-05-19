d7d4ae10fb22ec7846850bff93b9c129f6c0d474
Paul POUSSET <paul.pousset@student.junia.com>
2023-05-19 16:19:14 +0200

    members

--- src/pages/g/[id]/members.tsx
+++ src/pages/g/[id]/members.tsx
@@ -4,7 +4,7 @@ import { type NextPage, type GetStaticPaths, type GetStaticProps } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { FaTimes } from "react-icons/fa";
+import { FaTimes, FaCopy } from "react-icons/fa";
 import { AppHeader } from "~/components/header";
 import { type RouterOutputs, api } from "~/utils/api";
 
@@ -62,21 +62,30 @@ const GroupMembersPage: NextPage<{ groupId: string }> = ({ groupId }) => {
   const router = useRouter();
   ///
 
+  const copyToClipboard = (text) => {
+    const textField = document.createElement('textarea');
+    textField.innerText = text;
+    document.body.appendChild(textField);
+    textField.select();
+    document.execCommand('copy');
+    textField.remove();
+  };
+
   return (
     <>
       <Head>
         <title>{`Summer-Trip | Group members`}</title>
       </Head>
       <AppHeader groupId={groupId} groupName="Members" />
-      <main className="h-full flex-grow overflow-y-auto bg-[#405340] px-4 py-8">
-        <div id="membersList">
+      <main className="h-screen  bg-[#405340] px-4 pt-8 ">
+        <div id="membersList" className="border-2 border-[#E49A0A] rounded-lg p-4 mb-4">
           <h2 className="text-center  text-primary">Group members list</h2>
           <ul className="mt-4  text-primary">
             {members ? (
-              members.map((member) => (
+              members.map((member, index) => (
                 <li
                   key={member.id}
-                  className="item-center flex justify-between px-4 py-2"
+                  className={`item-center flex justify-between px-4 py-2 ${index !== members.length - 1 ? 'border-b border-[#E49A0A]' : ''}`}
                 >
                   <span>{member.name}</span>
                   <button
@@ -98,16 +107,23 @@ const GroupMembersPage: NextPage<{ groupId: string }> = ({ groupId }) => {
             className="rounded-full bg-[#E49A0A] px-4 py-2 text-[#1E5552]"
             onClick={() => generateInvitationLink({ groupId })}
           >
-            Generate invitation link
+            Generate invitation links
           </button>
           <ul className="mt-4  text-primary">
             {invitationLinks ? (
-              invitationLinks.map((inviteLink) => (
+              invitationLinks.map((inviteLink, index) => (
                 <li
                   key={inviteLink.id}
-                  className="item-center flex justify-between px-4 py-2"
+                  className="item-center flex justify-between  py-2"
                 >
-                  <span>{`${router.basePath}/i/${inviteLink.link}`}</span>
+                  <span className="text-sm sm:text-base">{`https://summertrip.fr${router.basePath}/i/${inviteLink.link}`}</span>
+                  <div>
+                    
+                      <button onClick={() => copyToClipboard(`https://summertrip.fr${router.basePath}/i/${inviteLink.link}`)}>
+                        <FaCopy className="h-4 w-4 text-primary" />
+                      </button>
+                    
+                  </div>
                 </li>
               ))
             ) : (
