fcfd6f5ea9ee8395ad3dd75e28f6762581bd6178
Bafbi <bafbi3@gmail.com>
2023-05-22 12:40:58 +0200

    invite page

--- src/pages/i/[slug].tsx
+++ src/pages/i/[slug].tsx
@@ -16,26 +16,47 @@ export const getStaticPaths: GetStaticPaths = () => {
   return { paths: [], fallback: "blocking" };
 };
 
-
 const InvitePage: NextPage<{ inviteLink: string }> = ({ inviteLink }) => {
-
-  const router = useRouter();
-
   useSession({
     required: true,
   });
+  const router = useRouter();
 
-  const { data: invitationData, isLoading: invitationLoading } = api.group.useInvitationLink.useQuery(
-    { link: inviteLink }
+  const { data: invitationData, isLoading: invitationLoading } =
+    api.group.getGroupByInvitationLink.useQuery({ link: inviteLink });
+
+  const { mutate } = api.group.useInvitationLink.useMutation({
+    onSuccess: () => {
+      void router.push(`/g`);
+    },
+  });
+
+  return (
+    <>
+      <main className="flex h-screen flex-col items-center justify-center bg-[#40534D]">
+        <div className="flex flex-col items-center justify-center gap-4">
+          <h1 className="text-4xl font-bold text-[#E49A0A]">SUMMER TRIP</h1>
+          <p className="text-2xl font-semibold text-[#E49A0A]">Invitation</p>
+          {invitationLoading ? (
+            <p>Loading...</p>
+          ) : !invitationData ? (
+            <p>Something went wrong</p>
+          ) : (
+            <p className="text-2xl font-semibold text-[#E49A0A]">
+              Vous avez été invité à rejoindre le groupe {invitationData.group.name}
+            </p>
+          )}
+          <button
+            className="rounded-lg bg-[#1E5552] px-4 py-2 text-lg font-semibold text-[#E49A0A] disabled:opacity-50 hover:bg-[#1CCDB3]"
+            onClick={() => void mutate({link: inviteLink})}
+            {...(invitationLoading || !invitationData ? { disabled: true } : {})}
+          >
+            Rejoindre le groupe
+          </button>
+        </div>
+      </main>
+    </>
   );
-
-  if (invitationLoading) return <div>Loading...</div>;
-
-  if (invitationData) {
-    void router.push(`/g`);
-  }
-
-  return <div>Something went wrong</div>;
 };
 
 export default InvitePage;
