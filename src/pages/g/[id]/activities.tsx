e2c795a021a0e381bdea4c55327db274048d0c4a
Julien DE ALMEIDA <julien.de-almeida@student.junia.com>
2023-05-22 13:15:28 +0200

    css activitiestsx

--- src/pages/g/[id]/activities.tsx
+++ src/pages/g/[id]/activities.tsx
@@ -1,8 +1,9 @@
-import { GetStaticPaths, GetStaticProps, type NextPage } from "next";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import { useSession } from "next-auth/react";
 import Head from "next/head";
 import { api } from "~/utils/api";
 import { AppHeader } from "~/components/header";
+import { FaHeart } from 'react-icons/fa';
 
 export const getStaticProps: GetStaticProps = (context) => {
   const groupId = context.params?.id as string;
@@ -11,53 +12,46 @@ export const getStaticProps: GetStaticProps = (context) => {
       groupId,
     },
   };
-}
+};
 
 export const getStaticPaths: GetStaticPaths = () => {
   return { paths: [], fallback: "blocking" };
-}
+};
 
-const ActivitiesPage: NextPage<{groupId: string}> = ({groupId}) => {
+const ActivitiesPage: NextPage<{ groupId: string }> = ({ groupId }) => {
   useSession({ required: true });
 
-  const { data: activityData, isLoading: activityLoading } = api.activity.getActivitiesWithVotes.useQuery( { groupId });
+  const { data: activityData, isLoading: activityLoading } = api.activity.getActivitiesWithVotes.useQuery({ groupId });
 
   if (activityLoading) return <div>Loading... {groupId}</div>;
 
   if (!activityData) return <div>Something went wrong</div>;
-
-
-  // const onSubmit = (data: any) => {
-  //   console.log(data);
-  //   setIsOpen(false);
-  // };
 
   return (
     <>
       <Head>
         <title>Groups</title>
       </Head>
-      <div className="flex flex-col h-screen bg-[#40534D]">
-      {/* Header */}
-      <AppHeader groupId={groupId} groupName={"Activities"}/>
+      <div className="flex flex-col min-h-screen bg-[#40534D]">
+        {/* Header */}
+        <AppHeader groupId={groupId} groupame={"Activities"} />
 
-      <main className="mt-8">
-        <div className="grid grid-cols-2 gap-4">
+        <main className="mt-8">
+          <div className="grid grid-cols-2 gap-4">
             {activityData.map((activity) => (
-              <div
-                key={activity.id}
-                className="overflow-hidden rounded-lg bg-white shadow"
-              >
-                <span>{activity.place.name}</span>
-                <span>{activity.voteCount}</span>
+              <div key={activity.id} className="overflow-hidden rounded-lg bg-[#E49A0A] shadow p-4 text-[#1E5552]">
+                <h3 className="text-xl font-bold">{activity.place.name}</h3>
+                <div className="flex items-center mt-2 text-black">
+                  <FaHeart className="h-4 w-4 mr-1 text-black font-bold" />
+                  <span className="text-black font-bold">{activity.voteCount} Likes</span>
+                </div>
               </div>
             ))}
           </div>
-      </main>
+        </main>
       </div>
     </>
   );
 };
 
 export default ActivitiesPage;
-
