0c6304c433ca5468f15aa007dc6b51c85aeb5420
Bafbi <bafbi3@gmail.com>
2023-05-24 16:49:44 +0200

    +

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,6 @@ const Home: NextPage = () => {
       <Head>
         <title>Summer Trip</title>
         <meta name="description" content="Summer TRIP" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-[#1E5552]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
