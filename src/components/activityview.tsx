1d939fa57e7dc3ecf143556fcdaa6cef32aa8aca
Bafbi <bafbi3@gmail.com>
2023-05-24 17:37:44 +0200

    no null message, loading activity

--- src/components/activityview.tsx
+++ src/components/activityview.tsx
@@ -6,114 +6,116 @@ import RatingStars from "./ratingstartview";
 
 const ActivityComponent: React.FC<{ groupId: string }> = ({ groupId }) => {
   const [activity, setActivity] = useState<RouterOutputs["activity"]["getActivityToRate"]>();
-  const [nextActivity, setNextActivity] = useState<RouterOutputs["activity"]["getActivityToRate"]>();
-  const [isLoading, setIsLoading] = useState(false);
 
-  const { mutate: activityMutation } = api.activity.getActivityToRate.useMutation({
+  const { mutate: activityMutation, isLoading } = api.activity.getActivityToRate.useMutation({
     onSuccess: (activity) => {
-      if (!nextActivity) {
-        setActivity(activity);
-      } else {
-        setActivity(nextActivity);
-        setNextActivity(activity);
-      }
-    },
-    onSettled: () => {
-      setIsLoading(false);
+      setActivity(activity);
     },
   });
 
-  const handleNextActivity = () => {
-    setIsLoading(true);
-    activityMutation({ groupId });
-  };
-
   useEffect(() => {
-    handleNextActivity();
+    activityMutation({ groupId });
   }, []);
 
-  const { mutate: rateActivity } = api.activity.rateActivity.useMutation();
+  const { mutate: rateActivity } = api.activity.rateActivity.useMutation({
+    onSuccess: () => {
+      activityMutation({ groupId });
+    },
+  });
 
-  const rateAndFetchNextActivity = (activityId: string, rating: number) => {
+  const handleRateActivity = (activityId: string, rating: number) => {
     rateActivity({ activityId, rating });
-    activityMutation({ groupId });
   };
 
   const generateEuroSymbols = (priceLevel: number) => {
-    let symbols = '';
-    for(let i = 0; i < priceLevel; i++) {
-      symbols += '€';
+    let symbols = "";
+    for (let i = 0; i < priceLevel; i++) {
+      symbols += "€";
     }
     return symbols;
   };
 
   return (
     <div className="flex flex-col items-center">
-      <h2 className="text-center text-3xl font-bold text-[#E49A0A] pt-3">
+      <h2 className="pt-3 text-center text-3xl font-bold text-[#E49A0A]">
         {"Ça t'interesse ?"}
       </h2>
-      <div className="max-w-md w-full mx-auto">
-        {isLoading ? (
-          <h3 className="text-center text-2xl font-bold text-[#E49A0A]">
-            Chargement ...
-          </h3>
-        ) : (
-          activity && (
-            <>
-              <div className="flex justify-center mt-3">
-                <div className="relative w-80 h-80">
-                  <div className="w-full h-full rounded-lg overflow-hidden shadow-xl">
-                    <div className="aspect-w-1 aspect-h-1">
-                      <Image 
-                        src={`data:image/png;base64,${activity.photo}`}
-                        alt="Activity Image"
-                        layout="fill"
-                        objectFit="cover"
-                        className="rounded-lg shadow-xl"
-                      />
-                    </div>
+      <div className="mx-auto w-full max-w-md">
+        {!isLoading && activity ? (
+          <>
+            <div className="mt-3 flex justify-center">
+              <div className="relative h-80 w-80">
+                <div className="h-full w-full overflow-hidden rounded-lg shadow-xl">
+                  <div className="aspect-w-1 aspect-h-1">
+                    <Image
+                      src={`data:image/png;base64,${activity.photo}`}
+                      alt="Activity Image"
+                      layout="fill"
+                      objectFit="cover"
+                      className="rounded-lg shadow-xl"
+                    />
                   </div>
                 </div>
               </div>
-              <div className="flex flex-col justify-center items-center">
-                <p className="text-center text-2xl font-bold pt-3 text-[#E49A0A]">
-                  {activity.place.name}
-                </p>
-                <p className="flex justify-center mt-0 items-center text-2xl font-bold text-[#E49A0A]">
-                  <RatingStars rating={activity.place.rating ?? 0} />
-                </p>
-                <p className="flex justify-center mt-2 items-center text-2xl font-bold text-[#E49A0A]">
-                  {generateEuroSymbols(activity.place.priceLevel ?? 0)}
-                </p>
-                <hr className="w-4/5 mt-2 mb-2 border-[#E49A0A] border-2" />
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <p className="pt-3 text-center text-2xl font-bold text-[#E49A0A]">
+                {activity.place.name}
+              </p>
+              <p className="mt-0 flex items-center justify-center text-2xl font-bold text-[#E49A0A]">
+                <RatingStars rating={activity.place.rating ?? 0} />
+              </p>
+              <p className="mt-2 flex items-center justify-center text-2xl font-bold text-[#E49A0A]">
+                {generateEuroSymbols(activity.place.priceLevel ?? 0)}
+              </p>
+              <hr className="mb-2 mt-2 w-4/5 border-2 border-[#E49A0A]" />
+            </div>
+          </>
+        ) : (
+          <div className="mt-3 flex justify-center">
+            <div className="relative h-80 w-80">
+              <div className="h-full w-full overflow-hidden rounded-lg shadow-xl">
+                <div className="aspect-w-1 aspect-h-1">
+                  <svg
+                    className="h-full w-full text-gray-300"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      clipRule="evenodd"
+                      d="M19.414 4.586a2 2 0 010 2.828L6.828 19.414a2 2 0 01-2.828-2.828L16.586 4.586a2 2 0 012.828 0z"
+                      fillRule="evenodd"
+                    />
+                  </svg>
+                </div>
               </div>
-            </>
-          )
+            </div>
+          </div>
         )}
       </div>
-      <div className="flex mt-5">
+      <div className="absolute bottom-24 mt-5 flex">
         <button
-          {...(activity ? {} : { disabled: true })}
-          onClick={() => rateAndFetchNextActivity(activity?.id as string, -1)}
+          disabled={isLoading}
+          onClick={() => handleRateActivity(activity?.id as string, -1)}
           className="flex h-24 w-24 items-center justify-center rounded-full bg-[#E49A0A] text-white"
         >
           <FaTimes className="h-10 w-10" />
         </button>
         <button
-          {...(activity ? {} : { disabled: true })}
-          onClick={() => rateAndFetchNextActivity(activity?.id as string, 0)}
+          disabled={isLoading}
+          onClick={() => handleRateActivity(activity?.id as string, 0)}
           className="mx-6 flex h-24 w-24 items-center justify-center rounded-full bg-gray-400 text-white"
         >
           <FaMeh className="h-12 w-12" />
         </button>
         <button
-          {...(activity ? {} : { disabled: true })}
-          onClick={() => rateAndFetchNextActivity(activity?.id as string, 1)}
+          disabled={isLoading}
+          onClick={() => handleRateActivity(activity?.id as string, 1)}
           className="flex h-24 w-24 items-center justify-center rounded-full bg-red-500 text-white"
         >
           <FaHeart className="h-10 w-10" />
         </button>
-      </div> 
+      </div>
     </div>
   );
 };
